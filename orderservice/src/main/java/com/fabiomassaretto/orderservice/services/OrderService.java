7b21f18e317f25ce78850d096872269b9df44479
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-04-17 05:28:00 -0300

    Create kafka producer and topics for orders

--- orderservice/src/main/java/com/fabiomassaretto/orderservice/services/OrderService.java
+++ orderservice/src/main/java/com/fabiomassaretto/orderservice/services/OrderService.java
@@ -6,11 +6,13 @@ import com.fabiomassaretto.orderservice.domains.OrderResponse;
 import com.fabiomassaretto.orderservice.domains.enums.OrderStatus;
 import com.fabiomassaretto.orderservice.domains.mappers.OrderMapper;
 import com.fabiomassaretto.orderservice.exceptions.OrderNotFoundException;
+import com.fabiomassaretto.orderservice.kafka.producers.KafkaProducer;
 import com.fabiomassaretto.orderservice.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +22,13 @@ import java.util.List;
 @Slf4j
 public class OrderService {
     private static final Logger LOG = LoggerFactory.getLogger(OrderService.class);
+
+    private final KafkaProducer kafkaProducer;
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+
+    @Value("${kafka.topic.orders}")
+    private String TOPIC_ORDERS;
 
     public OrderResponse create(OrderRequest orderRequest) {
         OrderEntity entity = orderMapper.mapOrderRequestToEntity(orderRequest);
@@ -31,6 +38,13 @@ public class OrderService {
         OrderEntity orderEntity = orderRepository.save(entity);
 
         LOG.info("Order created: {}", orderEntity);
+
+        // produce to topic orders
+        String orderId = String.valueOf(orderEntity.getId());
+
+        kafkaProducer.simpleSend(TOPIC_ORDERS, "orderId", orderId);
+
+        LOG.info("OrderId sent to topic: {} with message: {}", TOPIC_ORDERS, orderId);
 
         return orderMapper.mapOrderEntityToResponse(orderEntity);
     }
